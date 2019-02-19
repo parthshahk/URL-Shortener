6fbc2aaa803fe224cb6d5c2dc0f757b5b6f5d017
Parth Shah <parthshahk@gmail.com>
2019-02-19 17:53:00 +0530

    Homepage Designed

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts/app')
+
+@section('content')
+
+    <div class="jumbotron">
+
+        <h1 class="display-4 text-center">URL Shortener</h1>
+            <p class="lead text-center">Measure your links with URL Shortener, a simple link tracking platform.</p>
+        <hr class="my-4">
+        <br><br>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm col-12 text-center lead">                    
+                    <i class="fa fa-link jumbo-icon" aria-hidden="true"></i><br><br>
+                    Shorten and optimize your links across devices, channels and platforms. Share them to get insights.
+                </div>
+                <div class="col-sm col-12 text-center lead">
+                    <i class="fa fa-line-chart jumbo-icon" aria-hidden="true"></i><br><br>
+                    Track individual link analytics and measure usage in each location.
+                </div>
+                <div class="col-sm col-12 text-center lead">
+                    <i class="fa fa-refresh jumbo-icon" aria-hidden="true"></i><br><br>
+                    Collect insights on your performance and optimize your marketing efforts to better fit your audience needs.
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+@endsection
